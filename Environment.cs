1b6f5ccdfd164ba7873097a5db729c7c6cc13b7a
Markus Rodler <Markus.Rodler@live.de>
2022-02-12 18:16:18 +0100

    ✨ Init repository

---
+++ Environment.cs
@@ -0,0 +1,18 @@
+namespace Dark.Console;
+
+public static class Environment
+{
+    public static ImmutableDictionary<string, string> Config
+    {
+        get
+        {
+            Dictionary<string, string> config = new();
+            foreach (var argument in GetCommandLineArgs())
+            {
+                var splittedArg = argument.Split('=', 2, StringSplitOptions.RemoveEmptyEntries);
+                config.Add(splittedArg[0], splittedArg.Length == 2 ? splittedArg[1] : "");
+            }
+            return config.ToImmutableDictionary();
+        }
+    }
+}
